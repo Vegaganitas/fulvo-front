399e992afb9e273ba805a987581ec3dd70ced225
mate <matemorinigo@gmail.com>
2024-12-27 21:10:33 -0300

    Tournament components and essentials created.

---
+++ src/components/tournament-table/TournamentFilters.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TournamentFilters = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TournamentFilters;
---
+++ src/components/tournament-table/TournamentStats.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TournamentStats = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TournamentStats;
